908255d073ac6cb3978e1bae03a278ac9d1c675d
Mayowa-hash <109809660+Mayowa-hash@users.noreply.github.com>
2022-07-26 09:06:58 +0100

    Update pass.sh

--- pass.sh
+++ pass.sh
@@ -1,34 +1,34 @@
 # excel
 ##perform
 
-#firstname="Mayowa"
-#lastname="Olasope"
-#echo "$firstname $lastname"
-#echo "$firstname"
-#echo "$lastname"
-#mkdir mayowa
-#mkdir biocomputing cd biocomputing
-#wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
-#wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
-#wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
-#mv wildtype.fna ../mayowa/
-#rm wildtype.gbk
-#grep 'tatatata' wildtype.fna
-#grep 'tatatata' wildtype.fna >mayowa2
-#clear 
-#ls && ls biocomputing 
+firstname="Mayowa"
+lastname="Olasope"
+echo "$firstname $lastname"
+echo "$firstname"
+echo "$lastname"
+mkdir mayowa
+mkdir biocomputing cd biocomputing
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+mv wildtype.fna ../mayowa/
+rm wildtype.gbk
+grep 'tatatata' wildtype.fna
+grep 'tatatata' wildtype.fna >mayowa2
+clear 
+ls && ls biocomputing 
 
 
 
-#figlet Mayowa
-#mkdir compare
-#wget https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz
-#gunzip unix_intro_data.tar.gz
-#tar unix_intro_data.tar.gz
-#cat seqmonk_genomes/Saccharomyces cerevisiae/EF4 
-#grep rRNAs Mito.dat
-#cp Mito.dat compare
-#mv Mito.dat Mitochondrion.txt
-#wc -l lane8_DD_P4_TTAGGC_L008_R1.fastq.gz
-#lp wc-l fastq.gz
-#w file name.gz
+figlet Mayowa
+mkdir compare
+wget https://www.bioinformatics.babraham.ac.uk/training/Introduction%20to%20Unix/unix_intro_data.tar.gz
+gunzip unix_intro_data.tar.gz
+tar unix_intro_data.tar.gz
+cat seqmonk_genomes/Saccharomyces cerevisiae/EF4 
+grep rRNAs Mito.dat
+cp Mito.dat compare
+mv Mito.dat Mitochondrion.txt
+wc -l lane8_DD_P4_TTAGGC_L008_R1.fastq.gz
+lp wc-l fastq.gz
+w file name.gz
